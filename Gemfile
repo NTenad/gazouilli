c2ebbdf33d2b3f174fbffae045ca76e7b325ebff
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2013-07-08 12:46:00 +0200

    ADD ALL THE THINGS

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'active_support'
+gem 'activerecord'
+gem 'sqlite3'
+gem 'sinatra'
+gem 'sinatra-flash'
+gem 'twitter'
+
+gem 'guard-livereload'
+gem 'pry'
+gem 'rack-livereload'
+gem 'shotgun'
+gem 'thin'
+gem 'tux'
--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
+# encoding: utf-8
+
+require 'pry'
+require 'active_support'
 require 'active_record'
 require 'sinatra'
+require 'sinatra/flash'
 require 'twitter'
-require 'pry'
+
+#############################################################################
+# Setup
+#############################################################################
+
+enable :sessions
 
 Twitter.configure do |config|
-  config.consumer_key = "fowYkbskZpEwmLh0sbtF1w"
-  config.consumer_secret = "Zj1i9RkRFik1mrXNGnzsYdCSlQ6wcEuHvNVYpEzvo"
-  config.oauth_token = "1374547314-gflGRLXi2Hfq8tYspk5KkRyxHq5c3jI3PIYz1Jp"
-  config.oauth_token_secret = "HSHzwNCMDXP192ZEqKpIXzrp9uuZQtaKRMMInsyWk"
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
 end
 
 ActiveRecord::Base.establish_connection(
@@ -15,7 +25,32 @@ ActiveRecord::Base.establish_connection(
   database:  'db/gazouilli.db'
 )
 
+class User < ActiveRecord::Base
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :tweets
+end
+
+class Friendship < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :friend, :class_name => 'User'
+end
+
 class Tweet < ActiveRecord::Base
+  belongs_to :user
+end
+
+class Twitter::User; alias :screen_name :username; end
+
+helpers do
+  def current_user
+    username = session[:username]
+    User.find_by_username username
+  end
+
+  def parse_tweet(tweet)
+    tweet.gsub(/@(\w+)/,'<a href="/users/\1">@\1</a>')
+  end
 end
 
 get '/' do
@@ -28,9 +63,100 @@ get '/' do
   erb :tweets
 end
 
+# sign in
+get '/sign_in' do
+  if session[:user_id].nil?
+    erb :sign_in
+  else
+    flash[:error] = "Vous êtes déjà connecté !"
+    redirect back
+  end
+end
+
+# create session
+post '/authenticate' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by_username(username)
+
+  if user.password == password
+    session[:username] = user.username
+    flash[:notice] = "Bienvenue #{user.username} !"
+    redirect '/'
+  else
+    flash[:error] = "Authentification echouée"
+    redirect '/sign_in'
+  end
+end
+
+# logout
+get '/logout' do
+  session.clear
+  flash[:notice] = "Déconnecté"
+  redirect '/'
+end
+
+# user timeline
+get '/users/:username' do
+  username = params[:username]
+  user = User.find_by_username(username)
+  @tweets = user.tweets
+
+  erb :tweets
+end
+
 # create tweet
 post '/tweets' do
-  Tweet.create(text: params[:text])
+  if current_user
+    tweet = Tweet.create(text: params[:text])
+    current_user.tweets << tweet
 
-  redirect '/'
+    flash[:notice] = "Tweet envoyé !"
+    redirect '/'
+  else
+    session[:unsaved_tweet] = params[:tweet]
+    flash[:error] = "Veuillez vous connecter"
+    redirect '/sign_in'
+  end
+end
+
+get '/sign_up' do
+  erb :sign_up
+end
+
+post '/users' do
+  username = params[:username]
+  password = params[:password]
+  User.create(
+    :username => username,
+    :password => password
+  )
+  flash[:notice] = "Compte crée !"
+  redirect '/sign_in'
+end
+
+get '/users' do
+  @users = User.all
+  erb :users
+end
+
+post '/follow' do
+  username = params[:username]
+  followed = User.find_by_username(username)
+
+  current_user.friends << followed
+
+  flash[:notice] = "Vous suivez maintenant #{followed.username}"
+  redirect '/users'
+end
+
+post '/unfollow' do
+  username = params[:username]
+  unfollowed = User.find_by_username(username)
+
+  current_user.friends.delete unfollowed
+
+  flash[:notice] = "Vous ne suivez plus #{unfollowed.username}"
+  redirect '/users'
 end
