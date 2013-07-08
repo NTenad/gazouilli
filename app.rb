1ed23e9381df68d7ab21055f90a6248e2b50ee99
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2013-07-08 10:51:16 +0200

    tweets depuis Twitter

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'twitter'
+
+Twitter.configure do |config|
+  config.consumer_key = "fowYkbskZpEwmLh0sbtF1w"
+  config.consumer_secret = "Zj1i9RkRFik1mrXNGnzsYdCSlQ6wcEuHvNVYpEzvo"
+  config.oauth_token = "1374547314-gflGRLXi2Hfq8tYspk5KkRyxHq5c3jI3PIYz1Jp"
+  config.oauth_token_secret = "HSHzwNCMDXP192ZEqKpIXzrp9uuZQtaKRMMInsyWk"
+end
 
 get '/' do
-  "Juste un peu de texte"
+  @tweets = Twitter.user_timeline('simplonco')
+
+  erb :tweets
 end
